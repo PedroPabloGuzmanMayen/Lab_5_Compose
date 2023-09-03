13d25e018c354d80548303cfb66e1f60e56be8e8
PedroPabloGuzmanMayen <ppguzmanmayen@gmail.com>
2023-09-03 01:11:06 -0600

    Mian Screen

--- app/src/main/java/com/example/lab5_compose/MainScreen.kt
+++ app/src/main/java/com/example/lab5_compose/MainScreen.kt
@@ -1,50 +1,71 @@
 package com.example.lab5_compose
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
+import androidx.compose.ui.unit.dp
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun mainScreen(navController: NavController){
-    Scaffold{
-        bodyCOntent(navController)
-    }
+fun mainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Text("Todo Eventos")
+            })
+        },
+        content = {
+            bodyContent()
+        }
+    )
 }
+
 @Composable
-fun bodyCOntent(navController: NavController){
-    Text("Ai")
-    Spacer(modifier = Modifier)
-    LazyVerticalGrid(columns = GridCells.Fixed(2), content ={
-        items(100){ i->
-            Box(modifier = Modifier
-                .padding(8.dp)
-                .aspectRatio(1f)
-                .clip(RoundedCornerShape(5.dp))){
-                Text("Hola")
+fun bodyContent() {
+    LazyVerticalGrid(columns = GridCells.Fixed(2), content = {
+        items(100) { i ->
+            Card(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .aspectRatio(1f)
+                    .clip(RoundedCornerShape(5.dp))
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    // Replace with your image resource
+                    Image(
+                        painter = painterResource(id = R.drawable.your_image_resource),
+                        contentDescription = null,
+                        modifier = Modifier.size(64.dp)
+                    )
+                    Text(
+                        text = "Hello",
+                        fontWeight = FontWeight.Bold
+                    )
+                }
             }
         }
-    } )
+    })
 }
 
-
-
-
+@Preview
+@Composable
+fun mainScreenPreview() {
+    mainScreen()
+}
