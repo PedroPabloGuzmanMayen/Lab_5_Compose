80269648fe56a0f3fda296d2e06f7f80b900889f
PedroPabloGuzmanMayen <ppguzmanmayen@gmail.com>
2023-09-22 08:04:23 -0600

    details

--- app/src/main/java/com/example/lab5_compose/details.kt
+++ app/src/main/java/com/example/lab5_compose/details.kt
@@ -1,22 +1,61 @@
 package com.example.lab5_compose
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material3.Card
-import androidx.compose.material3.Text
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 
 @Composable
+@OptIn(ExperimentalMaterial3Api::class)
 fun details(information: concertDetails){
-    content(information)
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+    Scaffold(
+        topBar = { //Appbar
+            TopAppBar(
+                title = {
+                    Text("Detalles") //Appname
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Yellow ),
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Localized description"
+                        )
+
+                    }
+                },
+                scrollBehavior = scrollBehavior
+            )
+        }
+    ) { innerPadding ->
+        Box(modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize()
+            .fillMaxWidth()
+            .fillMaxHeight()){
+            content(information)
+        }
+    }
 }
 
 @Composable
 fun content(information: concertDetails){
-    Card(){
+    Card(modifier = Modifier
+        .fillMaxSize(),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        )
+    ){
         Row(){
             Text(information.date)
+            Spacer(modifier = Modifier.width(8.dp))
             Text(information.hour)
 
         }
